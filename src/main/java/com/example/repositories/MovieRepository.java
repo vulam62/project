e45c9399d2d807db94d8e16fd2a3ad4a41cd437d
lehungdhcnhn <61101277+lehungdhcnhn@users.noreply.github.com>
2020-08-04 08:50:12 +0700

    pom

---
+++ src/main/java/com/example/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.example.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+}
